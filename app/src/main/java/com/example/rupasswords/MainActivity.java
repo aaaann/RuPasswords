ac03f0bb29eb28924ea7500ca23e39a945773c0d
Anne Terechowa <annevonwolffen@gmail.com>
2019-09-30 21:18:17 +0300

    some new things

--- app/src/main/java/com/example/rupasswords/MainActivity.java
+++ app/src/main/java/com/example/rupasswords/MainActivity.java
@@ -6,13 +6,13 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.TestLooperManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,9 +22,17 @@ public class MainActivity extends Activity {
     private TextView resultTextView;
     private View copyButton;
 
-    private CompoundButton checkCaps;
+    private TextView passwordQualityTextView;
+    private ImageView passwordQualityIndicator;
 
+    private CompoundButton checkCaps;
+    private CompoundButton checkNumbers;
+    private CompoundButton checkSymbols;
+
+    private TextView genResultTextView;
+    private View copyGenPassButton;
     private Button generateButton;
+
 
     private String[] russians;
     private String[] latin;
@@ -36,16 +44,30 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sourceEditText = findViewById(R.id.edti_source);
+        sourceEditText = findViewById(R.id.edit_source);
         resultTextView = findViewById(R.id.text_result);
         copyButton = findViewById(R.id.buttonCopyPassword);
-        checkCaps = findViewById(R.id.big_letters);
+
+        passwordQualityTextView = findViewById(R.id.tv_password_quality);
+        passwordQualityIndicator = findViewById(R.id.iv_pass_quality_indicator);
+
+
+        checkCaps = findViewById(R.id.caps_letters);
+        checkCaps = findViewById(R.id.numbers);
+        checkCaps = findViewById(R.id.symbols);
+        genResultTextView = findViewById(R.id.text_result_gen);
+        copyGenPassButton = findViewById(R.id.btn_copy_gen_pass);
         generateButton = findViewById(R.id.generate_password_button);
 
         russians = getResources().getStringArray(R.array.russians);
         latin = getResources().getStringArray(R.array.latin);
 
         helper = new PasswordsHelper(russians, latin);
+
+        copyButton.setEnabled(false);
+        copyGenPassButton.setEnabled(false);
+        passwordQualityTextView.setText(helper.getPasswordQuality("").getQualityString());
+        passwordQualityIndicator.getDrawable().setLevel(helper.getPasswordQuality("").getQualityLevel());
 
         sourceEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -58,6 +80,11 @@ public class MainActivity extends Activity {
                 resultTextView.setText(helper.convert(s));
 
                 copyButton.setEnabled(s.length() > 0);
+
+                passwordQualityTextView.setText(helper.getPasswordQuality(s).getQualityString());
+                passwordQualityIndicator.getDrawable().setLevel(helper.getPasswordQuality(s).getQualityLevel());
+
+
             }
 
             @Override
@@ -76,13 +103,31 @@ public class MainActivity extends Activity {
             }
         });
 
+        copyGenPassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                assert manager != null;
+                manager.setPrimaryClip(ClipData.newPlainText(getString(R.string.clipboard_title),genResultTextView.getText().toString()));
+                Toast.makeText(MainActivity.this, R.string.copied, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         generateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean useCaps = checkCaps.isChecked();
+                boolean useNumbers = checkCaps.isChecked();
+                boolean useSymbols = checkCaps.isChecked();
+
+                genResultTextView.setText(helper.generatePassword(useCaps, useNumbers,useSymbols, 8));
+
             }
         });
 
 
+
+
     }
+
 }
--- app/src/main/java/com/example/rupasswords/PasswordsHelper.java
+++ app/src/main/java/com/example/rupasswords/PasswordsHelper.java
@@ -1,6 +1,24 @@
 package com.example.rupasswords;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static com.example.rupasswords.PasswordQualityEnum.BAD;
+import static com.example.rupasswords.PasswordQualityEnum.EXCELLENT;
+import static com.example.rupasswords.PasswordQualityEnum.GOOD;
+import static com.example.rupasswords.PasswordQualityEnum.NORMAL;
+import static com.example.rupasswords.PasswordQualityEnum.VERY_BAD;
+import static com.example.rupasswords.PasswordQualityEnum.VERY_GOOD;
+
 public class PasswordsHelper {
+
+    private Map<Integer,String> passSymbols = new HashMap<Integer, String>(){{
+        put(1, "abcdefghijklmnopqrstuvwxyz");
+        put(2, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        put(3, "1234567890");
+        put(4, "!@#$%^&*()_+=-?<>~");
+    }};
 
     private final String[] russians;
     private final String[] latin;
@@ -38,6 +56,38 @@ public class PasswordsHelper {
 
         return result.toString();
     }
+
+    public PasswordQualityEnum getPasswordQuality(CharSequence password) {
+        if (password.length() < 4) return VERY_BAD;
+        else if (password.length() > 4 && password.length() <= 6) return BAD;
+        else if (password.length() > 6 && password.length() <= 8) return NORMAL;
+        else if (password.length() > 8 && password.length() <= 10) return GOOD;
+        else if (password.length() > 10 && password.length() <= 12) return VERY_GOOD;
+        else if (password.length() > 12) return EXCELLENT;
+
+        return VERY_BAD;
+    }
+
+
+    public String generatePassword(boolean useCaps, boolean useNumbers, boolean useSymbols, int passLength) {
+        StringBuilder result = new StringBuilder();
+
+        Random rnd = new Random();
+        String charsGroup;
+        int numberOfGroups = 1 + (useCaps ? 1 : 0) + (useNumbers ? 1 : 0) + (useSymbols ? 1 : 0);
+        for (int i = 1; i <= numberOfGroups; i++){
+            charsGroup = passSymbols.get(i);
+            result.append(charsGroup.charAt(rnd.nextInt(charsGroup.length())));
+        }
+        for (int i = 0; i < passLength - (numberOfGroups - 1); i++){
+            int whatGroup = rnd.nextInt(numberOfGroups);
+            charsGroup = passSymbols.get(whatGroup);
+            result.append(charsGroup.charAt(rnd.nextInt(charsGroup.length())));
+        }
+
+        return result.toString();
+    }
+
 
 
 }
